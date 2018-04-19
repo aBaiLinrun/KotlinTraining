9f84106b524e5d3dd11c1abce812c9f776a9fafb
Bai Linrun <bai.linrun@access-company.com>
2018-04-19 11:31:23 +0900

    Add fizzBuzz() Add fizzBuzzFree()

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -9,3 +9,43 @@ fun welcome() {
 fun hello() {
     println("Hello World!")
 }
+
+fun isDivideThree(n: Int): Boolean {
+    return n%3 == 0
+}
+
+
+fun isDivideFive(n: Int): Boolean {
+    return n%5 == 0
+}
+
+fun fizzBuzz() {
+    for (n in 1..100) {
+        // 実装する
+        // use if
+        if (isDivideThree(n)) {
+            println("Fizz")
+        } else if (isDivideFive(n)) {
+            println("Buzz")
+        } else {
+            println("FizzBuzz")
+        }
+        // use when
+        when (true) {
+            isDivideThree(n) -> println("Fizz")
+            isDivideFive(n) -> println("Buzz")
+            else -> println("FizzBuzz")
+        }
+    }
+}
+
+fun fizzBuzzFree() {
+    // 実装する
+    //val n = nextInt()
+    // use when
+    when (true) {
+        isDivideThree(n) -> println("Fizz")
+        isDivideFive(n) -> println("Buzz")
+        else -> println("FizzBuzz")
+    }
+}
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -4,6 +4,13 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class SampleTest {
+
+    @Test
+    fun testHello() {
+        // TODO: implement to call hello function
+        hello()
+    }
+
     @Test
     fun testWelcome() {
         welcome()
@@ -15,8 +22,30 @@ class SampleTest {
     }
 
     @Test
-    fun testHello() {
-        // TODO: implement to call hello function
-        hello()
+    fun testFizzBuzz() {
+        // TODO: implement to call fizzBuzz function
+        fizzBuzz()
+
+        // TODO: テストを書く
+        // assertEquals 等の assert 系 function を使おう
+        // 以下の kotlin.test パッケージのリファレンスも参照のこと
+        // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/index.html
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual, "[want] $expected [got] $actual")
+    }
+
+    @Test
+    fun testFizzBuzzFree() {
+        // TODO: implement to call fizzBuzzFree function
+        fizzBuzzFree()
+
+        // TODO: テストを書く
+        // assertEquals 等の assert 系 function を使おう
+        // 以下の kotlin.test パッケージのリファレンスも参照のこと
+        // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/index.html
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual, "[want] $expected [got] $actual")
     }
 }
